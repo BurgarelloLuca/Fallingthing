30acb483fb959d35dd7e90443c0dcbb1cc7476e3
BurgarelloLuca <burgarelloluca668@gmail.com>
2025-05-01 17:03:59 +0200

    modifiche 01/05 Movimento personaggio e caduta primo oggetto

--- FallingThing's/Form1.Designer.cs
+++ FallingThing's/Form1.Designer.cs
@@ -29,12 +29,70 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timerPersonaggio = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timerCadutaOggetti = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerPersonaggio
+            // 
+            this.timerPersonaggio.Interval = 10;
+            this.timerPersonaggio.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.Location = new System.Drawing.Point(504, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(470, 689);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 137);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timerCadutaOggetti
+            // 
+            this.timerCadutaOggetti.Enabled = true;
+            this.timerCadutaOggetti.Interval = 10;
+            this.timerCadutaOggetti.Tick += new System.EventHandler(this.timerCadutaOggetti_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1139, 822);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timerPersonaggio;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timerCadutaOggetti;
     }
 }
 
--- FallingThing's/Form1.cs
+++ FallingThing's/Form1.cs
@@ -12,9 +12,73 @@ namespace FallingThing_s
 {
     public partial class Form1: Form
     {
+        bool movimentoSinistra = false;
+        bool movimentoDestra = false;
+        int velocitaPersonaggio= 5;
+        PictureBox[] Immagini = new PictureBox[10];
+        Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+        
+            if(e.KeyCode == Keys.A)
+            {
+                movimentoSinistra = true;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                movimentoDestra = true;
+            }
+            timerPersonaggio.Start();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                movimentoSinistra = false;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                movimentoDestra = false;
+            }
+            if (!movimentoSinistra && !movimentoDestra)
+            {
+                timerPersonaggio.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int nuovaSinistra = pictureBox1.Left;
+            if (movimentoSinistra)
+            {
+                nuovaSinistra -= velocitaPersonaggio;
+            }
+            else if (movimentoDestra)
+            {
+                nuovaSinistra += velocitaPersonaggio;
+            }
+            if (nuovaSinistra>= 0 && nuovaSinistra + pictureBox1.Width <= this.ClientSize.Width)
+            {
+                pictureBox1.Left = nuovaSinistra;
+            }
+          
+        }
+
+        private void timerCadutaOggetti_Tick(object sender, EventArgs e)
+        {
+            int velocitaCaduta = 5;
+            pictureBox2.Top += velocitaCaduta;
+            if (pictureBox2.Top >= this.ClientSize.Height - pictureBox1.Height)
+            {
+                pictureBox2.Top = 0;
+            }
         }
     }
 }
